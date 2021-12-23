02dcb20d0996913210c22444787e0bcb1ec835ac
DrTeaParty <95947330+DrTeaParty@users.noreply.github.com>
2021-12-23 02:36:38 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+function largertext() {
+	txt = document.getElementById("chapter");
+     fontsize = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+     currentSize = parseFloat(fontsize);
+     txt.style.fontSize = (currentSize + 1) + 'px';
+	 
+	 lineheight = window.getComputedStyle(txt, null).getPropertyValue('line-height');
+	 currentHeight = parseFloat(lineheight);
+     txt.style.lineHeight = (currentHeight + 100) + 'px';
+	<!--document.getElementById("chapter").style.fontSize ++ "1px";->
+}
+
+
+
+function smallertext() {
+	txt = document.getElementById("chapter");
+     style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+     currentSize = parseFloat(style);
+     txt.style.fontSize = (currentSize - 1) + 'px';
+	<!--document.getElementById("chapter").style.fontSize ++ "1px";->
+}

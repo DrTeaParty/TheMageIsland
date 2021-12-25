4d845e928f6b4edd7b514345a2fc5d9f66a928d0
DrTeaParty <95947330+DrTeaParty@users.noreply.github.com>
2021-12-25 00:00:06 +0000

    Add files via upload
    Implemented js to handle the navbar, so won't need to change every one independently now.
    
    Added Author Page, and prettied up the buttons for text size changes.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+/*Button effects to increase and decrease text size*/
 function largertext() {
 	txt = document.getElementById("chapter");
      fontsize = window.getComputedStyle(txt, null).getPropertyValue('font-size');
@@ -11,12 +11,16 @@ function largertext() {
 	<!--document.getElementById("chapter").style.fontSize ++ "1px";->
 }
 
-
-
 function smallertext() {
 	txt = document.getElementById("chapter");
      style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
      currentSize = parseFloat(style);
      txt.style.fontSize = (currentSize - 1) + 'px';
 	<!--document.getElementById("chapter").style.fontSize ++ "1px";->
+}
+
+
+/*Navigation Bar*/
+function navigation(){
+	$("#nav-placeholder").load("html_includes/navigation.html");
 }
